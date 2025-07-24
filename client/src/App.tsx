8ca48f83c59ade30741a187debf1ce25ce970bdc
PhamThanhDat <104967900+datonefeed@users.noreply.github.com>
2025-07-24 17:00:15 +0700

    chore: add dependences

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,23 +1,9 @@
 import type React from "react"
-import { Routes, Route } from "react-router-dom"
-import MainLayout from "./layouts/MainLayout"
-import Dashboard from "./pages/Dashboard"
-import Products from "./pages/Products"
-import Orders from "./pages/Orders"
-import Customers from "./pages/Customers"
+import { Routes } from "react-router-dom"
+import AppRoutes from "./routes"
 
 const App: React.FC = () => {
-  return (
-    <MainLayout>
-      <Routes>
-        <Route path="/" element={<Dashboard />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/products" element={<Products />} />
-        <Route path="/orders" element={<Orders />} />
-        <Route path="/customers" element={<Customers />} />
-      </Routes>
-    </MainLayout>
-  )
+  return <Routes>{AppRoutes}</Routes>
 }
 
 export default App
